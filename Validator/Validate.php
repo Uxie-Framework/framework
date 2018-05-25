c5351f6f7f649745688e6e2910507072aca07387
Mohamed Amine <MohamedAmine1C@gmail.com>
2018-05-25 23:59:20 +0100

    fix errors genrating mechanism bug

--- Validator/Validate.php
+++ Validator/Validate.php
@@ -16,7 +16,7 @@ class validate
 
     public function isValide()
     {
-        return empty($this->errors) ? true : $this;
+        return empty($this->errors) ? true : false;
     }
 
     public function getErrors()
@@ -27,27 +27,23 @@ class validate
     private function generateErrorMessages(ErrorMessageGeneratorInterface $generator)
     {
         foreach ($this->errors as $error) {
-            $error = $this->bindField($error);
             $errorMessages[] = $generator->compileErrorMessage($error['validator'], $error['arguments']);
         }
-        return $errorMessages;
-    }
 
-    private function bindField(array $error)
-    {
-        $error['arguments'][0] = $this->field;
-        return $error;
+        return $errorMessages;
     }
 
     public function __call($validator, $arguments)
     {
         array_unshift($arguments, $this->input);
         if (!call_user_func_array(["Validator\Validators\\$validator", 'validate'], [$arguments])) {
+            $arguments[0] = $this->field;
             $this->errors[] = [
                 'validator' => $validator,
                 'arguments' => $arguments,
             ];
         }
+
         return $this;
     }
 }
