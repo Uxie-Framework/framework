79d7cfbe78a7a3680e3e2edc0b7575133cb09d88
Mohamed Amine <MohamedAmine1C@gmail.com>
2018-12-09 22:07:09 -0500

    Revert "remove unnecessary methods"
    This reverts commit bfe9d35e98562ab6a27f4af7b701bf6960eb0288.

--- Router/Route.php
+++ Router/Route.php
@@ -8,6 +8,7 @@ use MiddlewareHandler\Middleware as Middleware;
 class Route implements RouteInterface
 {
     private $action;
+    private $variables = [];
     private $routeUrl;
     private $method;
     private $middlewares;
@@ -27,6 +28,11 @@ class Route implements RouteInterface
         return $this->routeUrl;
     }
 
+    public function getVariables(): array
+    {
+        return $this->variables;
+    }
+
     public function getAction()
     {
         return $this->action;
@@ -35,6 +41,23 @@ class Route implements RouteInterface
     public function getMethod(): string
     {
         return $this->method;
+    }
+
+    public function setVariables(array $variables): void
+    {
+        $this->variables = $variables;
+    }
+
+    public function setRequest(Request $request): void
+    {
+        if ($this->method !== 'GET') {
+            $this->pushRequest($request);
+        }
+    }
+
+    private function pushRequest(Request $request): void
+    {
+        array_unshift($this->variables, $request);
     }
 
     public function bindMiddleware(string $middleware, bool $late): void
