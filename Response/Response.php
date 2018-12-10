3300bf96aa41acc651bb8fb3a2b24f2092f05c7c
Mohamed Amine <MohamedAmine1C@gmail.com>
2018-12-09 20:02:05 -0500

    create the response object

---
+++ Response/Response.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Response;
+
+class Response
+{
+    private $response;
+
+    public function write(string $text): Self
+    {
+        $response += $text;
+        return $this;
+    }
+
+    public function status(int $status): Self
+    {
+        http_response_code($status);
+        return $this;
+    }
+
+    public function end(): void
+    {
+        exit();
+    }
+
+    public function json(array $array): void
+    {
+        echo json_encode($array);
+    }
+
+    public function send(): Self
+    {
+        echo $response;
+        return $this;
+    }
+
+    public function view(string $view): Self
+    {
+        view($view);
+        return $this;
+    }
+
+    public function cookie(string $name, string $value, string $expirationDate): void
+    {
+        cookie($name, $value, $expirationDate);
+    }
+
+    public function clearCookies(string $cookie): void
+    {
+        unsetCookie($cookie);
+    }
+
+    public function session(string $name, string $value): void
+    {
+        session($name, $value);
+    }
+
+    public function clearSession(string $session): void
+    {
+        unsetSession($session);
+    }
+
+    public function back(): void
+    {
+        redirect(previousUrl());
+    }
+
+    public function refresh(): void
+    {
+        redirect(currentUrl());
+    }
+}
