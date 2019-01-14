b4b40557867520640741e514448947c2fc99201a
Mohamed Amine <MohamedAmine1C@gmail.com>
2019-01-14 10:29:39 -0500

    add response tests

---
+++ tests/response/ResponseTest.php
@@ -0,0 +1,48 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class ResponseTest extends TestCase
+{
+    public function setUp()
+    {
+        IOC\IOC::createContainer();
+
+        container()->bind('Response', function () {
+            return new Response\Response();
+        });
+    }
+
+    public function testWrite()
+    {
+        container()->Response->write('test');
+
+        $imageReflection = new ReflectionClass(container()->Response);
+        $property = $imageReflection->getProperty('response');
+        $property->setAccessible(true);
+        $response = $property->getValue(container()->Response);
+
+        $this->assertEquals($response, 'test');
+    }
+
+    public function testStatus()
+    {
+        container()->Response->status(22);
+        $this->assertEquals(http_response_code(), 22);
+    }
+
+    public function testJson()
+    {
+        container()->Response->json([
+        'name' => 'uxie',
+        'package' => 'response',
+      ]);
+
+        $imageReflection = new ReflectionClass(container()->Response);
+        $property = $imageReflection->getProperty('response');
+        $property->setAccessible(true);
+        $response = $property->getValue(container()->Response);
+
+        $this->assertEquals($response, '{"name":"uxie","package":"response"}');
+    }
+}
