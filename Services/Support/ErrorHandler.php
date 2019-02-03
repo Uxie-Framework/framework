e18dfc68370bd580e0d036377ad15b5df438385c
Mohamed Amine <MohamedAmine1C@gmail.com>
2019-02-03 16:05:33 -0500

    update error handling response

--- Services/Support/ErrorHandler.php
+++ Services/Support/ErrorHandler.php
@@ -13,6 +13,6 @@ class ErrorHandler
             $code = 'ERROR';
             $error = '404';
         }
-        container()->Response->unsetResponse()->view('CoreViews/error', ['code' => $code, 'error' => $error]);
+        container()->Response->unsetResponse()->view('CoreViews/error', ['code' => $code, 'error' => $error])->send();
     }
 }
