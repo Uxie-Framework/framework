f42d382f3b0eccf386bcc440e3a9c660a5d1bc1a
Mohamed Amine <MohamedAmine1C@gmail.com>
2019-05-06 14:44:23 -0400

    add all needed methods to session

--- Response/Session.php
+++ Response/Session.php
@@ -1,11 +1,13 @@
 <?php
-namespace Session;
+namespace Response;
 
 class Session
 {
     public function __construct()
     {
-        session_start();
+        if (!isset($_SESSION)) {
+            session_start();
+        }
     }
 
     public function set(string $key, string $value): void
@@ -15,16 +17,39 @@ class Session
 
     public function get(string $key): string
     {
+        if (!isset($_SESSION[$key])) {
+            throw new \Exception("$key does not exist in Session object", 1);
+        }
+
         return $_SESSION[$key];
     }
 
-    public function unsetSession(string $key): void
+    public function destroy(string $key): void
     {
         unset($_SESSION[$key]);
     }
 
-    public function unsetAllSessions(): void
+    public function destroyAll(): void
     {
         session_destroy();
+    }
+
+    public function __get(string $key): string
+    {
+        if (!isset($_SESSION[$key])) {
+            throw new \Exception("$key does not exist in Session object", 1);
+        }
+
+        return $_SESSION[$key];
+    }
+
+    public function __set(string $key, string $value): void
+    {
+        $_SESSION[$key] = $value;
+    }
+
+    public function __isset(string $key): bool
+    {
+        return isset($_SESSION[$key]) ? true : false;
     }
 }
