240a3b1f1d8685ca0dd1ee0035a05477eb7d9d4d
Mohamed Amine <MohamedAmine1C@gmail.com>
2019-05-05 20:36:59 -0400

    add session methods

--- Response/Session.php
+++ Response/Session.php
@@ -5,6 +5,26 @@ class Session
 {
     public function __construct()
     {
-        // code...
+        session_start();
+    }
+
+    public function set(string $key, string $value): void
+    {
+        $_SESSION[$key] = $value;
+    }
+
+    public function get(string $key): string
+    {
+        return $_SESSION[$key];
+    }
+
+    public function unsetSession(string $key): void
+    {
+        unset($_SESSION[$key]);
+    }
+
+    public function unsetAllSessions(): void
+    {
+        session_destroy();
     }
 }
