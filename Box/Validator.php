8e68d5044c4b2f21bf179b8e72993ade9c762aa1
Mohamed Amine <cheribet.amine@gmail.com>
2019-12-23 11:55:32 -0500

    add filter to Box validator & use lowercase for all

--- Box/Validator.php
+++ Box/Validator.php
@@ -4,7 +4,7 @@ namespace Box;
 
 class Validator
 {
-    private $commands = ['Controller', 'Model', 'Middleware', 'Repository'];
+    private $commands = ['controller', 'model', 'middleware', 'repository', 'filter'];
 
     public function validate($command)
     {
